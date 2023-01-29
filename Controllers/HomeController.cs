464d62d7f9855ca197689e5647d970d5a97484e1
prudvi-m <prudhvi.manukonda4@google.com>
2023-01-29 00:17:04 -0600

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quotation.Models;
+
+namespace Quotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = "";
+            ViewBag.FinalTotal = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(QuotationModel fv)
+        {
+            if (ModelState.IsValid)
+            {
+                // create culture variable
+                var culture = System.Globalization.CultureInfo
+                    .CreateSpecificCulture("en-US");
+
+                ViewBag.DiscountAmount = fv.DiscountAmount().ToString("c2", culture);
+                ViewBag.FinalTotal = fv.FinalTotal().ToString("c2", culture);
+            }
+            else
+            {
+                ViewBag.DiscountAmount = "";
+                ViewBag.FinalTotal = "";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Models/QuotationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Quotation.Models
+{
+    public class QuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a sale price.")]
+        [Range(0.1,9999999.0, ErrorMessage = "Subtotal Must be 0.1 or more.")]
+        public decimal? SubTotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(1, 100, ErrorMessage = "Discount must be between 1 and 100.")]
+        public decimal? Discount { get; set; }
+        public decimal DiscountAmount() => SubTotal > 0 ? (decimal) ((SubTotal / 100) * Discount) : 0;
+        public decimal FinalTotal() => ((decimal) (SubTotal - (DiscountAmount())));
+    }
+}
